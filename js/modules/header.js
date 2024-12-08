0c6dbb23a5173c1285df85c534d38627d50f8cd6
AndresB <andreesb995@gmail.com>
2024-12-08 14:13:30 -0500

    ajustando paths modulares

--- js/modules/header.js
+++ js/modules/header.js
@@ -2,15 +2,28 @@
 
 export function loadHeader() {
     const isIndex = window.location.pathname.endsWith("index.html") || window.location.pathname === "/";
-    
+
     const basePath = isIndex
-            ? "html/header.html" // Ruta para index.html
-            : "./header.html";
-    
-        fetch(basePath)
+        ? "html/header.html" // Ruta para index.html
+        : "./header.html";
+
+    fetch(basePath)
         .then(response => response.text())
         .then(data => {
-            document.getElementById('header-container').innerHTML = data;
+            const headerContainer = document.getElementById('header-container');
+            if (headerContainer) {
+                // Insertar el contenido del header
+                headerContainer.innerHTML = data;
+
+                // Ajustar los href de los enlaces
+                const links = headerContainer.querySelectorAll("a");
+                links.forEach(link => {
+                    const href = link.getAttribute("href");
+                    if (href && !href.startsWith("/")) {
+                        link.setAttribute("href", `../${href}`);
+                    }
+                });
+            }
         })
         .catch(error => console.error('Error cargando el header:', error));
 }
